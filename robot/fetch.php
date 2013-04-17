ef88f35f76148bb0ca9920b2f84a5115c054808b
Marc-Oliver Teschke <marcoliverteschke@mac.com>
2013-04-17 12:14:54 +0200

    Fetch-script removes duplicate items after the fact.

--- robot/fetch.php
+++ robot/fetch.php
@@ -61,6 +61,23 @@
 		$feed->last_fetch_finished = time();
 		R::store($feed);
 	}
+
+	// clean up duplicates
+	$dupes = R::getAll('SELECT guid, count(*) AS dupes FROM item GROUP BY guid HAVING dupes > 1');
 	
+	foreach($dupes as $dupe)
+	{
+		$items = R::find('item', ' guid = ? ', array($dupe['guid']));
+		$items_keys = array_keys($items);
+		$keep = $items_keys[0];
+		foreach($items as $key => $item)
+		{
+			if($key != $keep)
+			{
+				R::trash($item);
+			}
+		}
+	}
+
 	R::close();
 	echo "DB closed\n";
